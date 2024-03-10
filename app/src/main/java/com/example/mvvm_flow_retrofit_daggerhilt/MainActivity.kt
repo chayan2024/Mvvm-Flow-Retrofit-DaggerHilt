3e35551f6b35f0c18e8e1725165ce3eadcb959fc
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-10 19:37:31 +0530

    Dependecny has been added.

--- app/src/main/java/com/example/mvvm_flow_retrofit_daggerhilt/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_flow_retrofit_daggerhilt/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvm_flow_retrofit_daggerhilt.ui.theme.MvvmFlowRetrofitDaggerHiltTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/mvvm_flow_retrofit_daggerhilt/di/BaseApp.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm_flow_retrofit_daggerhilt.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApp : Application()
